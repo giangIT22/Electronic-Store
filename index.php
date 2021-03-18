60ad68d7c748b2d4109884dbbdf5241e298370e9
giang <giang22.fit@gmail.com>
2021-03-18 22:00:17 +0700

    add

--- index.php
+++ index.php
@@ -1,35 +1,36 @@
 <?php
-session_start();
-require './Core/UploadFile.php';
-require './Core/Database.php';
-require './Helper/Common.php';
-require './Models/BaseModel.php';
-require './Controllers/BaseController.php';
-$controllerName = ucfirst((strtolower($_REQUEST['controller'] ?? 'home')) .'Controller');
+// session_start();
+// require './Core/UploadFile.php';
+// require './Core/Database.php';
+// require './Helper/Common.php';
+// require './Models/BaseModel.php';
+// require './Controllers/BaseController.php';
+// $controllerName = ucfirst((strtolower($_REQUEST['controller'] ?? 'home')) .'Controller');
 
-$moduleName = $_GET['module'] ?? null;
+// $moduleName = $_GET['module'] ?? null;
 
-$actionName = $_REQUEST['action'] ?? 'index';
+// $actionName = $_REQUEST['action'] ?? 'index';
 
-if($moduleName === 'backend'){
-    $controllerFile = "./Controllers/Backend/${controllerName}.php";
-}else{
-    $controllerFile = "./Controllers/${controllerName}.php";
-}
+// if($moduleName === 'backend'){
+//     $controllerFile = "./Controllers/Backend/${controllerName}.php";
+// }else{
+//     $controllerFile = "./Controllers/${controllerName}.php";
+// }
 
-if(file_exists($controllerFile)){
-    require $controllerFile;
-}else{
-    echo "<h1>Không tìm thấy ${controllerName}</h1>";
-    die();
-}
+// if(file_exists($controllerFile)){
+//     require $controllerFile;
+// }else{
+//     echo "<h1>Không tìm thấy ${controllerName}</h1>";
+//     die();
+// }
 
-$object1 = new $controllerName;
+// $object1 = new $controllerName;
 
-if(method_exists($object1, $actionName)){
-    $object1->$actionName();
-}
-else{
-    echo "<h2>Không tìm thấy method: ${actionName} ở trong ${controllerName}</h2>";
-}
+// if(method_exists($object1, $actionName)){
+//     $object1->$actionName();
+// }
+// else{
+//     echo "<h2>Không tìm thấy method: ${actionName} ở trong ${controllerName}</h2>";
+// }
+echo " giang";
 
